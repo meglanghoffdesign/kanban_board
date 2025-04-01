ae38251476abd35857093c359a93522fd5da9a62
meglanghoffdesign <meglanghoff@gmail.com>
2025-03-31 21:04:16 -0600

    Added port to index.ts in models

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -10,6 +10,7 @@ const sequelize = process.env.DB_URL
   : new Sequelize(process.env.DB_NAME || '', process.env.DB_USER || '', process.env.DB_PASSWORD, {
       host: 'localhost',
       dialect: 'postgres',
+      port:5432,
       dialectOptions: {
         decimalNumbers: true,
       },
